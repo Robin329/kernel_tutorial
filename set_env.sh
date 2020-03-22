f709ba480a5b08f148502483b5e76fc403643141
Robin <jiangrenbin329@gmail.com>
2020-03-22 02:35:53 -0400

    Delete some dont need files.
    Signed-off-by: Robin <jiangrenbin329@gmail.com>

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 #
-# set Makefile env for arm64
+# set Linux-lab env for Robin
+# Version:
+#		2020.01.09 add base enviroments with vim .bashrc.
 #
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
+# export ARCH=arm64
+# export CROSS_COMPILE=aarch64-linux-gnu-
+apt update -y
+apt upgrade -y
+apt install man -y
+apt install python3-dev libssl-dev -y
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp -rvf /labs/linux-lab/workspace/configs/.vim ~/
+cp -rvf /labs/linux-lab/workspace/configs/.bashrc ~/
+cp -rvf /labs/linux-lab/workspace/configs/.vimrc ~/
