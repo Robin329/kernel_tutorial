3d66e3ebe5d12a98b1519a9347eb046af1bfa93c
robin <jiangrenbin329@gmail.com>
2022-07-14 08:06:57 -0700

    add usb driver
    Signed-off-by: robin <jiangrenbin329@gmail.com>

--- robin_lab/lab23_linked_list/linked_list_demo.c
+++ robin_lab/lab23_linked_list/linked_list_demo.c
@@ -17,23 +17,23 @@
 #include <linux/module.h>
 #include <linux/slab.h> //kmalloc()
 #include <linux/sysfs.h>
-#include <linux/uaccess.h>   //copy_to/from_user()
+#include <linux/uaccess.h> //copy_to/from_user()
 #include <linux/workqueue.h> // Required for workqueues
 
 #define pr_fmt(fmt) KBUILD_MODNAME ":[%s:%d] " fmt, __func__, __LINE__
 
-#define IRQ_NO 11
+#define IRQ_NO 0
 
-volatile int etx_value = 0;
+volatile int rb_value = 0;
 
 dev_t dev = 0;
 static struct class *dev_class;
-static struct cdev etx_cdev;
+static struct cdev rb_cdev;
 struct kobject *kobj_ref;
 static struct workqueue_struct *own_workqueue;
 
-static int __init etx_driver_init(void);
-static void __exit etx_driver_exit(void);
+static int __init rb_driver_init(void);
+static void __exit rb_driver_exit(void);
 
 static void workqueue_fn(struct work_struct *work);
 
@@ -41,8 +41,8 @@ static DECLARE_WORK(work, workqueue_fn);
 
 /*Linked List Node*/
 struct my_list {
-    struct list_head list; // linux kernel list implementation
-    int data;
+	struct list_head list; // linux kernel list implementation
+	int data;
 };
 
 /*Declare and init the head node of the linked list*/
@@ -52,212 +52,233 @@ LIST_HEAD(Head_Node);
 ** Function Prototypes
 */
 /*************** Driver functions **********************/
-static int etx_open(struct inode *inode, struct file *file);
-static int etx_release(struct inode *inode, struct file *file);
-static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
-static ssize_t etx_write(struct file *filp, const char *buf, size_t len, loff_t *off);
+static int rb_open(struct inode *inode, struct file *file);
+static int rb_release(struct inode *inode, struct file *file);
+static ssize_t rb_read(struct file *filp, char __user *buf, size_t len,
+		       loff_t *off);
+static ssize_t rb_write(struct file *filp, const char *buf, size_t len,
+			loff_t *off);
 
 /*************** Sysfs functions **********************/
-static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf);
-static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count);
+static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr,
+			  char *buf);
+static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr,
+			   const char *buf, size_t count);
 
-struct kobj_attribute etx_attr = __ATTR(etx_value, 0660, sysfs_show, sysfs_store);
+struct kobj_attribute rb_attr = __ATTR(rb_value, 0660, sysfs_show, sysfs_store);
 /******************************************************/
 
 /*Workqueue Function*/
-static void workqueue_fn(struct work_struct *work) {
-    struct my_list *temp_node = NULL;
+static void workqueue_fn(struct work_struct *work)
+{
+	struct my_list *temp_node = NULL;
 
-    pr_info("Executing Workqueue Function\n");
+	pr_info("Executing Workqueue Function\n");
 
-    /*Creating Node*/
-    temp_node = kmalloc(sizeof(struct my_list), GFP_KERNEL);
+	/*Creating Node*/
+	temp_node = kmalloc(sizeof(struct my_list), GFP_KERNEL);
 
-    /*Assgin the data that is received*/
-    temp_node->data = etx_value;
+	/*Assgin the data that is received*/
+	temp_node->data = rb_value;
 
-    /*Init the list within the struct*/
-    INIT_LIST_HEAD(&temp_node->list);
+	/*Init the list within the struct*/
+	INIT_LIST_HEAD(&temp_node->list);
 
-    /*Add Node to Linked List*/
-    list_add_tail(&temp_node->list, &Head_Node);
+	/*Add Node to Linked List*/
+	list_add_tail(&temp_node->list, &Head_Node);
 }
 
 // Interrupt handler for IRQ 11.
-static irqreturn_t irq_handler(int irq, void *dev_id) {
-    pr_info("Shared IRQ: Interrupt Occurred\n");
-    /*Allocating work to queue*/
-    queue_work(own_workqueue, &work);
+static irqreturn_t irq_handler(int irq, void *dev_id)
+{
+	pr_info("Shared IRQ: Interrupt Occurred\n");
+	/*Allocating work to queue*/
+	queue_work(own_workqueue, &work);
 
-    return IRQ_HANDLED;
+	return IRQ_HANDLED;
 }
 
 /*
 ** File operation sturcture
 */
 static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .read = etx_read,
-    .write = etx_write,
-    .open = etx_open,
-    .release = etx_release,
+	.owner = THIS_MODULE,
+	.read = rb_read,
+	.write = rb_write,
+	.open = rb_open,
+	.release = rb_release,
 };
 
 /*
 ** This function will be called when we read the sysfs file
 */
-static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) {
-    pr_info("Sysfs - Read!!!\n");
-    return sprintf(buf, "%d", etx_value);
+static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr,
+			  char *buf)
+{
+	pr_info("Sysfs - Read!!!\n");
+	return sprintf(buf, "%d", rb_value);
 }
 
 /*
 ** This function will be called when we write the sysfsfs file
 */
-static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count) {
-    pr_info("Sysfs - Write!!!\n");
-    return count;
+static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr,
+			   const char *buf, size_t count)
+{
+	pr_info("Sysfs - Write!!!\n");
+	return count;
 }
 
 /*
 ** This function will be called when we open the Device file
 */
-static int etx_open(struct inode *inode, struct file *file) {
-    pr_info("Device File Opened...!!!\n");
-    return 0;
+static int rb_open(struct inode *inode, struct file *file)
+{
+	pr_info("Device File Opened...!!!\n");
+	return 0;
 }
 
 /*
 ** This function will be called when we close the Device file
 */
-static int etx_release(struct inode *inode, struct file *file) {
-    pr_info("Device File Closed...!!!\n");
-    return 0;
+static int rb_release(struct inode *inode, struct file *file)
+{
+	pr_info("Device File Closed...!!!\n");
+	return 0;
 }
 
 /*
 ** This function will be called when we read the Device file
 */
-static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
-    struct my_list *temp;
-    int count = 0;
-    pr_info("Read function\n");
+static ssize_t rb_read(struct file *filp, char __user *buf, size_t len,
+		       loff_t *off)
+{
+	struct my_list *temp;
+	int count = 0;
+	pr_info("Read function\n");
 
-    /*Traversing Linked List and Print its Members*/
-    list_for_each_entry(temp, &Head_Node, list) { pr_info("Node %d data = %d\n", count++, temp->data); }
+	/*Traversing Linked List and Print its Members*/
+	list_for_each_entry (temp, &Head_Node, list) {
+		pr_info("Node %d data = %d\n", count++, temp->data);
+	}
 
-    pr_info("Total Nodes = %d\n", count);
-    return 0;
+	pr_info("Total Nodes = %d\n", count);
+	return 0;
 }
 
 /*
 ** This function will be called when we write the Device file
 */
-static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
-    pr_info("Write Function\n");
-    /*Copying data from user space*/
-    // sscanf(buf, "%d", &etx_value);
-    if (copy_from_user(&etx_value, buf, sizeof(etx_value))) {
-        pr_err("Data Write : Err!\n");
-        return -EFAULT;
-    }
-    /* Triggering Interrupt */
-    // asm("int $0x3B"); // Corresponding to irq 11
-    enable_irq(IRQ_NO);
-    return len;
+static ssize_t rb_write(struct file *filp, const char __user *buf, size_t len,
+			loff_t *off)
+{
+	pr_info("Write Function\n");
+	/*Copying data from user space*/
+	// sscanf(buf, "%d", &rb_value);
+	if (copy_from_user(&rb_value, buf, sizeof(rb_value))) {
+		pr_err("Data Write : Err!\n");
+		return -EFAULT;
+	}
+	/* Triggering Interrupt */
+	// asm("int $0x3B"); // Corresponding to irq 11
+	// enable_irq(IRQ_NO);
+	return len;
 }
 
 /*
 ** Module Init function
 */
-static int __init etx_driver_init(void) {
-    /*Allocating Major number*/
-    if ((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) < 0) {
-        pr_err("Cannot allocate major number\n");
-        return -1;
-    }
-    pr_info("Major = %d Minor = %d n", MAJOR(dev), MINOR(dev));
+static int __init rb_driver_init(void)
+{
+	/*Allocating Major number*/
+	if ((alloc_chrdev_region(&dev, 0, 1, "rb_dev")) < 0) {
+		pr_err("Cannot allocate major number\n");
+		return -1;
+	}
+	pr_info("Major = %d Minor = %d n", MAJOR(dev), MINOR(dev));
 
-    /*Creating cdev structure*/
-    cdev_init(&etx_cdev, &fops);
+	/*Creating cdev structure*/
+	cdev_init(&rb_cdev, &fops);
 
-    /*Adding character device to the system*/
-    if ((cdev_add(&etx_cdev, dev, 1)) < 0) {
-        pr_err("Cannot add the device to the system\n");
-        goto r_class;
-    }
+	/*Adding character device to the system*/
+	if ((cdev_add(&rb_cdev, dev, 1)) < 0) {
+		pr_err("Cannot add the device to the system\n");
+		goto r_class;
+	}
 
-    /*Creating struct class*/
-    if ((dev_class = class_create(THIS_MODULE, "etx_class")) == NULL) {
-        pr_err("Cannot create the struct class\n");
-        goto r_class;
-    }
+	/*Creating struct class*/
+	if ((dev_class = class_create(THIS_MODULE, "rb_class")) == NULL) {
+		pr_err("Cannot create the struct class\n");
+		goto r_class;
+	}
 
-    /*Creating device*/
-    if ((device_create(dev_class, NULL, dev, NULL, "etx_device")) == NULL) {
-        pr_err("Cannot create the Device \n");
-        goto r_device;
-    }
+	/*Creating device*/
+	if ((device_create(dev_class, NULL, dev, NULL, "rb_device")) == NULL) {
+		pr_err("Cannot create the Device \n");
+		goto r_device;
+	}
 
-    /*Creating a directory in /sys/kernel/ */
-    kobj_ref = kobject_create_and_add("etx_sysfs", kernel_kobj);
+	/*Creating a directory in /sys/kernel/ */
+	kobj_ref = kobject_create_and_add("rb_sysfs", kernel_kobj);
 
-    /*Creating sysfs file*/
-    if (sysfs_create_file(kobj_ref, &etx_attr.attr)) {
-        pr_err("Cannot create sysfs file......\n");
-        goto r_sysfs;
-    }
-    if (request_irq(IRQ_NO, irq_handler, IRQF_SHARED, "etx_device", (void *)(irq_handler))) {
-        pr_err("my_device: cannot register IRQ \n");
-        goto irq;
-    }
+	/*Creating sysfs file*/
+	if (sysfs_create_file(kobj_ref, &rb_attr.attr)) {
+		pr_err("Cannot create sysfs file......\n");
+		goto r_sysfs;
+	}
+	if (request_irq(IRQ_NO, irq_handler, IRQF_SHARED, "rb_device",
+			(void *)(irq_handler))) {
+		pr_err("my_device: cannot register IRQ \n");
+		goto irq;
+	}
 
-    /*Creating workqueue */
-    own_workqueue = create_workqueue("own_wq");
+	/*Creating workqueue */
+	own_workqueue = create_workqueue("own_wq");
 
-    pr_info("Device Driver Insert...Done!!!\n");
-    return 0;
+	pr_info("Device Driver Insert...Done!!!\n");
+	return 0;
 
 irq:
-    free_irq(IRQ_NO, (void *)(irq_handler));
+	free_irq(IRQ_NO, (void *)(irq_handler));
 
 r_sysfs:
-    kobject_put(kobj_ref);
-    sysfs_remove_file(kernel_kobj, &etx_attr.attr);
+	kobject_put(kobj_ref);
+	sysfs_remove_file(kernel_kobj, &rb_attr.attr);
 
 r_device:
-    class_destroy(dev_class);
+	class_destroy(dev_class);
 r_class:
-    unregister_chrdev_region(dev, 1);
-    cdev_del(&etx_cdev);
-    return -1;
+	unregister_chrdev_region(dev, 1);
+	cdev_del(&rb_cdev);
+	return -1;
 }
 
 /*
 ** Module Exit function
 */
-static void __exit etx_driver_exit(void) {
-    /* Go through the list and free the memory. */
-    struct my_list *cursor, *temp;
-    list_for_each_entry_safe(cursor, temp, &Head_Node, list) {
-        list_del(&cursor->list);
-        kfree(cursor);
-    }
+static void __exit rb_driver_exit(void)
+{
+	/* Go through the list and free the memory. */
+	struct my_list *cursor, *temp;
+	list_for_each_entry_safe (cursor, temp, &Head_Node, list) {
+		list_del(&cursor->list);
+		kfree(cursor);
+	}
 
-    /* Delete workqueue */
-    destroy_workqueue(own_workqueue);
-    free_irq(IRQ_NO, (void *)(irq_handler));
-    kobject_put(kobj_ref);
-    sysfs_remove_file(kernel_kobj, &etx_attr.attr);
-    device_destroy(dev_class, dev);
-    class_destroy(dev_class);
-    cdev_del(&etx_cdev);
-    unregister_chrdev_region(dev, 1);
-    pr_info("Device Driver Remove...Done!!\n");
+	/* Delete workqueue */
+	destroy_workqueue(own_workqueue);
+	free_irq(IRQ_NO, (void *)(irq_handler));
+	kobject_put(kobj_ref);
+	sysfs_remove_file(kernel_kobj, &rb_attr.attr);
+	device_destroy(dev_class, dev);
+	class_destroy(dev_class);
+	cdev_del(&rb_cdev);
+	unregister_chrdev_region(dev, 1);
+	pr_info("Device Driver Remove...Done!!\n");
 }
 
-module_init(etx_driver_init);
-module_exit(etx_driver_exit);
+module_init(rb_driver_init);
+module_exit(rb_driver_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("robin.jiang <jiangrenbin123@gmail.com>");
