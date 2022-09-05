c7f405d39950f1caffc6934d154e46e80bcb44e8
robin <jiangrenbin329@gmail.com>
2022-09-04 18:13:42 -0700

    add qcom mipi dsi debug patch
    Signed-off-by: robin <jiangrenbin329@gmail.com>

--- project/qcom/linux/lab1_hello/hello.c
+++ project/qcom/linux/lab1_hello/hello.c
@@ -7,7 +7,12 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ":[%s:%d] " fmt, __func__, __LINE__
 
 static int __init modules_init(void) {
+    int32_t ctl_reg = 0x00000604;
     pr_info("Module init\n");
+    ctl_reg |= BIT(10);
+    ctl_reg |= 7 << 7;
+    ctl_reg |= 0 & GENMASK(1, 0) << 5;
+    pr_err("genmask:%#x\n", ctl_reg);
     return 0;
 }
 
